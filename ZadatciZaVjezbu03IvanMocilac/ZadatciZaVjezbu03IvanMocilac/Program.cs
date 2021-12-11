9caaa1339f8b8f1508f898694ee1ccf7ee41097f
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-12-11 17:59:52 +0100

    Add files via upload

---
+++ ZadatciZaVjezbu03IvanMocilac/ZadatciZaVjezbu03IvanMocilac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu03IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 9, 16, 7, 5, 17, 4, 14, 39, 1 };
+            Sort sort = new Sort();
+            sort.QuickSort(arr, 1, arr.Length - 2);
+            for (int i = 0; i < arr.Length; i++){
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
